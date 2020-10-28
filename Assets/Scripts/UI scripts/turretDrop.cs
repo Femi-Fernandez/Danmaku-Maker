6bc7a2dba7c88ed3fa2b7bfc3e3544ba0c5d5297
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2020-10-28 16:29:57 +0000

    begun working on UI, able to drag and drop objects onto the game scene

---
+++ Assets/Scripts/UI scripts/turretDrop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class turretDrop : MonoBehaviour, IDropHandler
+{
+    public GameObject turret;
+    public GameObject boss;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        RectTransform turretPanel = transform as RectTransform;
+        //throw new System.NotImplementedException();
+        if (!RectTransformUtility.RectangleContainsScreenPoint(turretPanel, Input.mousePosition))
+        {
+            Debug.Log("object dragged from panel!");
+
+            Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameObject spawnedTurret = Instantiate(turret, worldPosition, transform.rotation) as GameObject;
+
+            spawnedTurret.transform.parent = boss.transform;
+        }
+    }
+}
