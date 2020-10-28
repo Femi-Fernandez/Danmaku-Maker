005c257a164433fc535f3f643cbb3fb95dd72ec4
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2020-10-28 12:56:28 +0000

    created player with temporary sprites, and created a simple bullet prefab

---
+++ Assets/Scripts/Player scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	private float speed;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		if (Input.GetKey(KeyCode.LeftShift))
+		{
+			speed = 5f;
+		}
+		else
+		{
+			speed = 10f;
+		}
+		transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0) * speed * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/Player scripts/Player_focusMode.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_focusMode : MonoBehaviour
+{
+    //public GameObject barrel_center;
+    public GameObject barrel_left;
+    public GameObject barrel_right;
+    //public GameObject barrel_left_boost;
+    //public GameObject barrel_right_boost;
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            
+            barrel_left.transform.eulerAngles = new Vector3(-90, 90, 90);
+            //barrel_left_boost.transform.eulerAngles = new Vector3(-90, 90, 90);
+            barrel_right.transform.eulerAngles = new Vector3(-90, 90, 90);
+            //barrel_right_boost.transform.eulerAngles = new Vector3(-90, 90, 90);
+        } 
+        else
+        {
+            barrel_left.transform.eulerAngles = new Vector3(-95, 90, 90);
+            //barrel_left_boost.transform.eulerAngles = new Vector3(-95, 90, 90);
+            barrel_right.transform.eulerAngles = new Vector3(-85, 90, 90);
+            //barrel_right_boost.transform.eulerAngles = new Vector3(-85, 90, 90);
+        }
+    }
+}
