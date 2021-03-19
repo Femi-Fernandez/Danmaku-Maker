abca4efa54917a65229ddc50cf2125f356346664
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2021-03-19 17:39:36 +0000

    attempting to get turret rotation implemented

---
+++ Assets/Scripts/Turret scripts/turretSpriteManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretSpriteManager : MonoBehaviour
+{
+
+    public int rotateState;
+    Transform rotateDir;
+    public Transform parentRot;
+
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotateDir = transform.parent.GetChild(0).transform;
+        //parentRot = transform.parent.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = parentRot.rotation;
+        rotateState = Convert.ToInt32(rotateDir.eulerAngles.z / 45.0f);
+
+        anim.SetInteger("direction", rotateState);
+    }
+}
