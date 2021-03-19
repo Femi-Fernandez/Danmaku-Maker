c180cc5ad8657d71334ed6874da80b98d554477c
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2021-03-19 17:51:33 +0000

    turret sprite script working

--- Assets/Scripts/Turret scripts/turretSpriteManager.cs
+++ Assets/Scripts/Turret scripts/turretSpriteManager.cs
@@ -8,21 +8,22 @@ public class turretSpriteManager : MonoBehaviour
 
     public int rotateState;
     Transform rotateDir;
-    public Transform parentRot;
+    //public Transform parentRot;
 
     public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        rotateDir = transform.parent.GetChild(0).transform;
+        //rotateDir = transform.parent.GetChild(0).transform;
         //parentRot = transform.parent.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = parentRot.rotation;
-        rotateState = Convert.ToInt32(rotateDir.eulerAngles.z / 45.0f);
+
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, gameObject.transform.parent.rotation.z * -1.0f);
+        rotateState = Convert.ToInt32(gameObject.transform.parent.rotation.eulerAngles.z / 45.0f);
 
         anim.SetInteger("direction", rotateState);
     }
