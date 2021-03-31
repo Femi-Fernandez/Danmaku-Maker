3e3d97aec925cd376a728d8b3f5151f5e7486555
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2021-03-31 19:46:37 +0100

    begun work on background

---
+++ Assets/Scripts/Misc/backgroundControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int anim;
+    //public GameObject[] baseShapes;
+    public GameObject[] morphShapes;
+    public float timer;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 90 * Time.deltaTime, 0); //rotates 50 degrees per second around z axis
+        timer += Time.deltaTime;
+
+        if (timer > 5.0f)
+        {
+            StartCoroutine(triggerAnim(0));
+            timer = -30.0f;
+        }
+    }
+
+    IEnumerator triggerAnim(int morphNum)
+    {
+        //baseShapes[morphNum].SetActive(false);
+
+        morphShapes[morphNum].GetComponent<Animator>().SetInteger("play_anim", 1);
+        yield return StartCoroutine("animationDelay");
+
+        //baseShapes[morphNum + 1].SetActive(true);
+    }
+
+    IEnumerator animationDelay()
+    {
+        yield return new WaitForSeconds(1);
+    }
+}
