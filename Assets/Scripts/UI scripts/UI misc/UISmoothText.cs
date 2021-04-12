6c4eff0ec31feab0ece69d7799e3f236fb2b2d25
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2021-04-12 16:38:33 +0100

    Created dropdown box artwork, created a script to make text not blur

---
+++ Assets/Scripts/UI scripts/UI misc/UISmoothText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISmoothText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Text thisText;
+
+    RectTransform rect;
+    int numToMultiply;
+    void Start()
+    {
+        numToMultiply = 5;
+        thisText = GetComponent<Text>();
+        rect = GetComponent<RectTransform>();
+
+        thisText.fontSize = thisText.fontSize * numToMultiply;
+        transform.localScale = transform.localScale / numToMultiply;
+        rect.sizeDelta = new Vector2(rect.sizeDelta.x * numToMultiply, rect.sizeDelta.y * numToMultiply);
+
+
+    }
+
+
+}
