a1fd7bbb69399188420480908a5719d43ee79e50
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2021-05-08 14:44:12 +0100

    extended barrel length of turret

--- Assets/Scripts/Boss scripts/GenerateBossTiles.cs
+++ Assets/Scripts/Boss scripts/GenerateBossTiles.cs
@@ -36,18 +36,10 @@ public class GenerateBossTiles : MonoBehaviour
             }
 
             linePoints.Add(new Vector2(0, 0));
-
             linePoints.Add(new Vector2(turretLocations[i].transform.position.x, turretLocations[i].transform.position.y - 2.5f));
-
-            //Debug.Log(turretLocations[i].transform.position.x + " " + turretLocations[i].transform.position.y);
-
-            //lineToCore[i] = new EdgeCollider2D();
-
             lineToCore.points = linePoints.ToArray();
 
             Vector3Int currentCells = bossTileMap.WorldToCell(turretLocations[i].transform.position);
-
-            //Debug.Log(currentCells);
             bossTileMap.SetTile(currentCells, smartTile);
         }
     }
